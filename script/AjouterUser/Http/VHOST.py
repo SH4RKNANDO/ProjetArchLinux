82c979590b6f6981d07b05a8117d4a793cf61f5f
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2019-05-06 18:33:12 +0200

    New Function

--- script/AjouterUser/Http/VHOST.py
+++ script/AjouterUser/Http/VHOST.py
@@ -34,7 +34,7 @@ class Vhost:
         print("*-------------------------------------------------------*")
 
     def _gestiondroit(self):
-        print("\nGestions des Droits sur le répertoire Web\n")
+        print("\nCréation du Répertoire Web")
         print(os.system("mkdir -pv " + self._webdirectory))
 
         directory = [
@@ -45,15 +45,18 @@ class Vhost:
             "/home/jail/home/" + self._username + "/public_html"
         ]
 
+        print("\nCopies du site dans le répertoire web")
         site = '"' + "<html><body><h1>Site en Construction</h1></body></html>" + '"'
         cmd = "echo " + site + " > /home/jail/home/" + self._username + "/public_html/index.html"
         print(os.system(cmd))
 
+        print("\nGestions des Droits sur le répertoire Web\n")
         for x in directory:
-            print(x)
+            print("Répertoire en cours : " + x)
             print(os.system("chmod -v 770 " + x))
             print(os.system("chown -v " + self._username + ":http " + x))
 
+        print("\nGestions des Droits sur le Fichier web")
         cmd = "chown -v " + self._username + ":http /home/jail/home/" + self._username + "public_html/index.html"
         print(os.system(cmd))
         print(os.system("chmod -v 1770 /home/jail/home/" + self._username + "/public_html/index.html"))
